2da12399c5573aa3bd3728fd4da887e1f872c48a
Eversoncooper <everson.coopeer@gmail.com>
2025-02-15 15:27:57 -0300

    Desafio conta bancária finalizado.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    String agencia;
+    String nomeCliente;
+    double saldo;
+
+           //Exibir as mensagens para o usuário
+           //Obter pela scanner os valores digitados no terminal 
+           Scanner sc = new Scanner(System.in);
+           System.out.println("Por favor, digite o número da agência: ");
+           agencia = sc.nextLine();
+        
+           System.out.println("Por favor, digite o nome do cliente: ");
+           nomeCliente = sc.nextLine();
+        
+           System.out.println("Por favor, digite o saldo inicial: ");
+           saldo = sc.nextDouble();
+
+           //Obter pela scanner os valores digitados no terminal
+           System.out.println("Seja bem-vindo " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " e seu saldo " + saldo + " já está disponível para saque.");
+
+           sc.close();
     }
 }
